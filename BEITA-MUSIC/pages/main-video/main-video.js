06ec787a9e399b3cceb869b90786b0a364183a34
beitaandbaozi <2280496040@qq.com>
2023-02-15 18:34:42 +0800

    feat: tabBar视频页面，下来刷新功能

--- BEITA-MUSIC/pages/main-video/main-video.js
+++ BEITA-MUSIC/pages/main-video/main-video.js
@@ -24,7 +24,7 @@ Page({
   // 获取所有的视频数据
   async getAllVedio() {
     // 1.获取数据
-    const res = await getTopMvList(20,this.data.offset)
+    const res = await getTopMvList(20, this.data.offset)
     // 2.拼接之前的数据
     const newVideoList = [...this.data.videoList, ...res.data]
     this.setData({
@@ -37,7 +37,26 @@ Page({
   // 上拉加载
   onReachBottom() {
     // 如果已经全部加载完成，就不去请求
-    if(!this.data.hasMore) return;
+    if (!this.data.hasMore) return;
     this.getAllVedio()
+  },
+
+  // 下拉刷新
+  async onPullDownRefresh() {
+    // 1.清空之前的数据
+    this.data.videoList = []
+    this.data.offset = 0
+    this.data.hasMore = true
+
+    // 使用this.setData({})改了js文件里面的data的值,页面中用到这个值的地方也会一起自动更换了,页面自动显示最新数据
+    // this.setData({
+    //   videoList:[],
+    //   offset:0,
+    //   hasMore:true
+    // })
+    // 2.重新请求数据
+    await this.getAllVedio()
+    // 3.等数据加载完成立即关闭下拉刷新的状态  ====> await 阻塞作用
+    wx.stopPullDownRefresh()
   }
 })
