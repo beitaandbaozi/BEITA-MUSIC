0331f997074052d7339f99445be44cacabe8ec36
beitaandbaozi <2280496040@qq.com>
2023-02-15 16:55:19 +0800

    feat: 视频tabBar页面，将每一个视频信息抽取为组件并完成样式渲染

---
+++ BEITA-MUSIC/components/video-item/video-item.js
@@ -0,0 +1,26 @@
+// components/video-item/video-item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    videoItem: {
+      type: Object,
+      default: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
