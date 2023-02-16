1006ed554205987c5ea0a8a33e9ae931e4ef7392
beitaandbaozi <2280496040@qq.com>
2023-02-16 15:57:10 +0800

    style: tabBar音乐页面，将区域头部封装成组件

---
+++ BEITA-MUSIC/components/area-header/area-header.js
@@ -0,0 +1,30 @@
+// components/area-header/area-header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      default: "默认标题"
+    },
+    hasMore: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
