172872bf72f4e626e706c3384d97b0426a4dc330
beitaandbaozi <2280496040@qq.com>
2023-02-18 18:24:23 +0800

    feat: tabBar音乐页面，推荐歌曲详细页渲染完成

--- BEITA-MUSIC/pages/detail-song/detail-song.js
+++ BEITA-MUSIC/pages/detail-song/detail-song.js
@@ -1,9 +1,12 @@
 import rankingStore from '../../store/ranking-store'
+import recommendStore from '../../store/recommend-store'
 Page({
   data: {
     type: 'ranking',
     key: 'newRanking',
-    musicList: []
+    musicList: [],
+    // 因为推荐歌单的接口其实是我接了热门歌单的 所以用一个变量来替代本来的名称
+    titleName: ''
   },
   onLoad(options) {
     // 通过路由中的type来判断展示对应的数据
@@ -16,6 +19,10 @@ Page({
     this.data.key = key
     if (type === 'ranking') {
       rankingStore.onState(key, this.handleStoreCallback)
+    } else if (type === 'recommend') {
+      this.data.type = type
+      this.data.titleName = '推荐歌曲'
+      recommendStore.onState("recommendMusicInfo", this.handleStoreCallback)
     }
 
   },
@@ -24,11 +31,17 @@ Page({
       musicList: value
     })
     // 设置导航标题
+    if (this.data.type === 'recommend') value.name = "推荐歌曲"
     wx.setNavigationBarTitle({
       title: value.name,
     })
   },
   onUnload() {
-    rankingStore.offState(this.data.key, this.handleStoreCallback)
+    if (this.data.type === 'ranking') {
+      rankingStore.offState(this.data.key, this.handleStoreCallback)
+    } else if (this.data.type === 'recommend') {
+      recommendStore.offState("recommendMusicInfo", this.handleStoreCallback)
+    }
+
   }
 })
--- BEITA-MUSIC/pages/main-music/main-music.js
+++ BEITA-MUSIC/pages/main-music/main-music.js
@@ -123,7 +123,7 @@ Page({
   // 跳转到更多页面
   handleToMoreMusic() {
     wx.navigateTo({
-      url: '/pages/detail-song/detail-song',
+      url: '/pages/detail-song/detail-song?type=recommend',
     })
   },
 
