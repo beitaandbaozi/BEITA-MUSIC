e5b8ad1ebe9dad30f0c50de604f0eecdca73cf89
beitaandbaozi <2280496040@qq.com>
2023-04-20 23:38:52 +0800

    feat: 使用数组将搜索tap组合起来渲染并创建对应的页面

--- BEITA-MUSIC/pages/detail-search/detail-search.js
+++ BEITA-MUSIC/pages/detail-search/detail-search.js
@@ -11,7 +11,25 @@ Page({
     // 当前播放的歌曲
     currentSong: {},
     // 播放状态
-    isPlaying: false
+    isPlaying: false,
+    // tap
+    tapGroup: [{
+        type: 'singer',
+        name: '歌手'
+      },
+      {
+        type: 'sing',
+        name: '曲风'
+      },
+      {
+        type: 'mv',
+        name: 'MV'
+      },
+      {
+        type: 'area',
+        name: '专区'
+      }
+    ]
   },
   /**
    * 生命周期函数--监听页面加载
@@ -53,6 +71,36 @@ Page({
   handleToSearch(event) {
     const keywords = event.currentTarget.dataset.keywords
     this.fetchSearch(keywords)
+  },
+  // 歌手、曲风、MV、专区点击跳转
+  handleToTapDetail(event) {
+    // 获取对应的分类
+    // 0：歌手  1：曲风  2：MV 3：专区
+    const type = event.currentTarget.dataset.type
+    switch (type) {
+      case 0:
+        wx.navigateTo({
+          url: '/pages/search-singer-detail/search-singer-detail',
+        })
+        break;
+      case 1:
+        wx.navigateTo({
+          url: '/pages/search-sing-detail/search-sing-detail',
+        })
+        break;
+      case 2:
+        wx.navigateTo({
+          url: '/pages/search-mv-detail/search-mv-detail',
+        })
+        break;
+      case 3:
+        wx.navigateTo({
+          url: '/pages/search-area-detail/search-area-detail',
+        })
+        break;
+      default:
+        break;
+    }
   },
   // =========================== store事件 ================
   handleGetPlaySongInfos({
